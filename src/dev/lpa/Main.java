6f2466f50c1d6dcbc198a8a19cb3b71ec88ad10b
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-13 18:01:34 +0200

    show Path instance methods, file does not exist yet

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,9 +1,27 @@
 package dev.lpa;
 
+import java.nio.file.Path;
+
 public class Main {
 
   public static void main(String[] args) {
 
+    Path path = Path.of("files/testing.txt");
+    printPathInfo(path);
 
+
+  }
+
+  private static void printPathInfo(Path path) {
+
+    System.out.println("Path: " + path);
+    System.out.println("fileName = " + path.getFileName());
+    System.out.println("parent = " + path.getParent());
+    Path absolutePath = path.toAbsolutePath();
+    System.out.println("Absolute Path: = " + absolutePath);
+    System.out.println("Absolute Path Root: = " + absolutePath.getRoot());
+    System.out.println("Root = " + path.getRoot()); // if null is relative path
+    System.out.println("isAbsolute = " + path.isAbsolute());
+    System.out.println("-------------------------------------------");
   }
 }
